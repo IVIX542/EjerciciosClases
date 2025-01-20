f29cdbe01ec9c195e805f7a6f221701fa46e070e
IVIX542 <ivix542@gmail.com>
2025-01-20 20:46:28 +0100

    Initial commit

---
+++ src/localidad/Localidad.java
@@ -0,0 +1,99 @@
+package localidad;
+
+public class Localidad {
+	
+	private String nombre;
+	private String provincia;
+	private int numHabitantes;
+	private int distanciaACapital;
+	private int superficie;
+	private int rentaPerCapita;
+	
+	public Localidad(String nombre, String provincia, int numHabitantes, int distanciaACapital, int superficie, int rentaPerCapita) {
+		this.nombre = nombre;
+		this.provincia = provincia;
+		this.numHabitantes = numHabitantes;
+		this.distanciaACapital = distanciaACapital;
+		this.superficie = superficie;
+		this.rentaPerCapita = rentaPerCapita;
+	}
+	
+	//Métodos
+	
+	public static void tieneMasPoblacion(Localidad localidad2) {
+		
+		Localidad localidad1 = new Localidad("Pinto", "Madrid", 56003, 20, 62, 100000);
+
+		if (localidad1.numHabitantes > localidad2.numHabitantes) {
+			System.out.println(localidad1.nombre + " tiene más población que " + localidad2.nombre);
+		} else {
+			System.out.println(localidad1.nombre + " tiene menos población que " + localidad2.nombre);
+		}
+		
+	}
+	
+	//Setters
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+	
+	public void setNumHabitantes(int numHabitantes) {
+		this.numHabitantes = numHabitantes;
+	}
+	
+	public void setDistanciaACapital(int distanciaACapital) {
+		this.distanciaACapital = distanciaACapital;
+	}
+	
+	public void setSuperficie(int superficie) {
+		this.superficie = superficie;
+	}
+	
+	public void setRentaPerCapita(int rentaPerCapita) {
+		this.rentaPerCapita = rentaPerCapita;
+	}
+	
+	//Getters
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public String getProvincia() {
+		return provincia;
+	}
+	
+	public int getNumHabitantes() {
+		return numHabitantes;
+	}
+	
+	public int getDistanciaACapital() {
+		return distanciaACapital;
+	}
+	
+	public int getSuperficie() {
+		return superficie;
+	}
+	
+	public int getRentaPerCapita() {
+		return rentaPerCapita;
+	}
+
+	public static void main(String[] args) {
+		
+		Localidad localidad2 = new Localidad("Valdemoro", "Madrid", 83507, 26, 64, 10000);
+		Localidad localidad3 = new Localidad("Aranjuez", "Madrid", 62292, 47, 201, 100000);
+		Localidad localidad4 = new Localidad("Madarcos", "Madrid", 70, 87, 8, 1000);
+		
+		System.out.println(localidad2.nombre + " está en " + localidad2.provincia + ", tiene " + localidad2.numHabitantes + " habitantes. Está a " + localidad2.distanciaACapital + " km de la capital, y tiene " + localidad2.superficie + " km cuadrados.");
+		
+		tieneMasPoblacion(localidad2);
+		tieneMasPoblacion(localidad3);
+		tieneMasPoblacion(localidad4);
+		
+	}
+
+}
